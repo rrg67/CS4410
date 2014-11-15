12dcab26917fc82ac83f259005620c73e5df6fa5
rrg67 <rrg67@cornell.edu>
2014-11-14 19:49:56 -0500

    Update server.py

--- server.py
+++ server.py
@@ -51,7 +51,6 @@ class ConnectionHandler:
         #self.completeMessage = (self.socket.recv(1024))
         if (self.state == None):
             self.socket.send(b"220 rrg67 SMTP CS4410MP3\r\n")
-            self.state = "Open"
         while (True):
             self.socket.settimeout(10)
             self.partialMessage = (self.socket.recv(1024))
@@ -61,28 +60,12 @@ class ConnectionHandler:
                     break
                 else: 
                     self.completeMessage = self.completeMessage + self.partialMessage
-            print("Complete " + self.completeMessage)
             # Waiting for a HELO command
             if (self.state == "Open"):
                 print ("state is open, looking for a HELO")
                 if (self.completeMessage[1:5] == "HELO"):
-                    m = checkNonWhiteSpace(self.completeMessage[5:])
-                    if (self.completeMessage[5] != " " and self.completeMessage[5:7] != "\r\n"):
-                            self.socket.send(b"500 Error: command not recognized\r\n")
-                    elif (self.completeMessage[5] != " "):
-                        self.socket.send(b"501 Syntax:  proper syntax\r\n")
-                    elif (self.completeMessage[m:m+2] == "\r\n"):
-                        self.socket.send(b"501 Syntax:  proper syntax\r\n")
-                    else :
-                        self.state = "HELO"
-                        self.socket.send(b"250 rrg67\r\n")
-                        self.state = None
-                        self.completeMessage = None
-                        #self.partialMessage = None
-                        #self.endMessage = False
-                else:
-                    self.socket.send(b"503 Error: need HELO command\r\n")
-                        # Waiting for a MAIL FROM command
+                    print("Complete " + self.completeMessage)
+                    
             
             
         
