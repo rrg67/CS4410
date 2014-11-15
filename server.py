fbd6a740d1c2679e5e9b2a709f562b39a3816170
rrg67 <rrg67@cornell.edu>
2014-11-14 19:43:08 -0500

    Update server.py

--- server.py
+++ server.py
@@ -56,13 +56,33 @@ class ConnectionHandler:
             self.socket.settimeout(10)
             self.partialMessage = (self.socket.recv(1024))
             i = 0
-            print("Partial " + self.partialMessage)
             while (i < len(self.partialMessage)):
                 if (self.partialMessage[i:i+4] == '\\r\\n'):
                     break
                 else: 
                     self.completeMessage = self.completeMessage + self.partialMessage
             print("Complete " + self.completeMessage)
+            # Waiting for a HELO command
+            if (self.state == "Open"):
+                print ("state is open, looking for a HELO")
+                if (self.completeMessage[1:5] == "HELO"):
+                    m = checkNonWhiteSpace(self.completeMessage[5:])
+                    if (self.completeMessage[5] != " " and self.completeMessage[5:7] != "\r\n"):
+                            self.socket.send(b"500 Error: command not recognized\r\n")
+                    elif (self.completeMessage[5] != " "):
+                        self.socket.send(b"501 Syntax:  proper syntax\r\n")
+                    elif (self.completeMessage[m:m+2] == "\r\n"):
+                        self.socket.send(b"501 Syntax:  proper syntax\r\n")
+                    else :
+                        self.state = "HELO"
+                        self.socket.send(b"250 rrg67\r\n")
+                        self.state = None
+                        self.completeMessage = None
+                        #self.partialMessage = None
+                        #self.endMessage = False
+                else:
+                    self.socket.send(b"503 Error: need HELO command\r\n")
+                        # Waiting for a MAIL FROM command
             
             
         
