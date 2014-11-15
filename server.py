2dc43171a8fd8ab8257d10fbcde23af1b895034e
rrg67 <rrg67@cornell.edu>
2014-11-14 19:00:22 -0500

    Update server.py

--- server.py
+++ server.py
@@ -48,7 +48,7 @@ class ConnectionHandler:
 
     def handle(self):
         # Lets the client know a connection has been made
-        self.completeMessage = (self.socket.recv(1024))
+        #self.completeMessage = (self.socket.recv(1024))
         print("this is shit")
         if (self.state == None):
             self.socket.send(b"220 rrg67 SMTP CS4410MP3\r\n")
